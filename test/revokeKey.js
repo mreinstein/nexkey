40b6f935e548a38da400c4150291c525aa1dc89b
Mike Reinstein <reinstein.mike@gmail.com>
2018-03-22 12:31:48 -0700

    basic unit tests

---
+++ test/revokeKey.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const proxyquire = require('proxyquire')
+const test       = require('tap').test
+
+
+const URL_PREFIX = 'https://nexkey-beta.herokuapp.com/rest/functions/'
+
+
+test('basic', async function(t) {
+  const nexkey = proxyquire('../', { 'node-fetch': function stubbedFetch(url, options) {
+    t.equal(url, URL_PREFIX + 'revokeKey')
+    return { json: function() { return {} }}
+  } })
+
+  const client = nexkey({ NEXKEY_API_SECRET: '123', NEXKEY_API_KEY: '456' })
+
+  await client.revokeKey({ })
+
+  t.end()
+})
